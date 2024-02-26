c3f981daff98bca5d70e2cb8f7e34c1fb2aa428e
Addisu <addisuhaile87@gmail.com>
2024-02-26 18:48:27 +0300

    Change filter icons

--- app/components/libs/categories.ts
+++ app/components/libs/categories.ts
@@ -1,15 +1,15 @@
 import {
+  Bird,
   Castle,
   Droplet,
   Fan,
+  Fish,
   Gem,
   Landmark,
   LucideIcon,
-  Mountain,
   MountainSnow,
   Palmtree,
   Sailboat,
-  Ship,
   Snowflake,
   Sun,
   Tent,
@@ -46,7 +46,7 @@ export const categories: CategoryProps[] = [
   {
     id: 4,
     label: "Countryside",
-    icon: Mountain,
+    icon: Bird,
     description: "This property is in the countryside!",
   },
   {
@@ -64,7 +64,7 @@ export const categories: CategoryProps[] = [
   {
     id: 7,
     label: "Lake",
-    icon: Ship,
+    icon: Fish,
     description: "This property is near a lake!",
   },
   {
