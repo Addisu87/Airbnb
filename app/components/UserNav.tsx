a9de0a2d75c60675d229fbefa0739dbdcf7dbbf8
Addisu <addisuhaile87@gmail.com>
2024-02-26 11:59:02 +0300

    Add logout functionlaity and update profile pic

--- app/components/UserNav.tsx
+++ app/components/UserNav.tsx
@@ -3,6 +3,7 @@ import { MenuIcon } from "lucide-react";
 import {
   RegisterLink,
   LoginLink,
+  LogoutLink,
 } from "@kinde-oss/kinde-auth-nextjs/components";
 import {
   DropdownMenu,
@@ -12,8 +13,12 @@ import {
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 
-const UserNav = () => {
+const UserNav = async () => {
+  const { getUser } = getKindeServerSession();
+  const user = await getUser();
+
   return (
     <div>
       <DropdownMenu>
@@ -21,18 +26,31 @@ const UserNav = () => {
           <MenuIcon className="w-6 h-6 lg:w-5 lg:h-5" />
 
           <Avatar className="hidden lg:block">
-            <AvatarImage src="https://static.vecteezy.com/system/resources/thumbnails/009/292/244/small/default-avatar-icon-of-social-media-user-vector.jpg" />
+            <AvatarImage
+              src={
+                user?.picture ??
+                "https://static.vecteezy.com/system/resources/thumbnails/009/292/244/small/default-avatar-icon-of-social-media-user-vector.jpg"
+              }
+            />
             <AvatarFallback>CN</AvatarFallback>
           </Avatar>
         </DropdownMenuTrigger>
         <DropdownMenuContent align="end" className="w-[200px]">
           <DropdownMenuSeparator />
-          <DropdownMenuItem>
-            <RegisterLink>Sign up</RegisterLink>
-          </DropdownMenuItem>
-          <DropdownMenuItem>
-            <LoginLink>Sign in</LoginLink>
-          </DropdownMenuItem>
+          {user ? (
+            <DropdownMenuItem>
+              <LogoutLink>Log out</LogoutLink>
+            </DropdownMenuItem>
+          ) : (
+            <>
+              <DropdownMenuItem>
+                <RegisterLink>Sign up</RegisterLink>
+              </DropdownMenuItem>
+              <DropdownMenuItem>
+                <LoginLink>Sign in</LoginLink>
+              </DropdownMenuItem>
+            </>
+          )}
 
           <DropdownMenuSeparator />
           <DropdownMenuItem>Airbnb your home</DropdownMenuItem>
