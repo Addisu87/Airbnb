3bbc21f9847ce1bc317f4290905fd52dd43dac65
Addisu <addisuhaile87@gmail.com>
2024-02-29 15:48:19 +0300

    Implement map functionlity

--- app/(routes)/rent/[id]/address/page.tsx
+++ app/(routes)/rent/[id]/address/page.tsx
@@ -1,27 +1,39 @@
+"use client";
+
+import { useState } from "react";
+import dynamic from "next/dynamic";
+
+import BottomBar from "@/app/components/BottomBar";
 import { useCountries } from "@/app/hooks/getCountries";
 import {
   Select,
   SelectContent,
   SelectGroup,
   SelectItem,
-  SelectLabel,
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
+import { Skeleton } from "@/components/ui/skeleton";
 
 export default function AddressRoute() {
   const { getAllCountries } = useCountries();
+  const [locationValue, setLocationValue] = useState("");
+
+  const LazyMap = dynamic(() => import("@/app/components/Map"), {
+    ssr: false,
+    loading: () => <Skeleton className="h-[50vh] w-full" />,
+  });
 
   return (
     <>
-      <div className="w-3/5 mx-auto">
+      <div className="w-3/5 mx-auto mb-36">
         <h2 className="text-2xl font-semibold tracking-tight transition-colors mb-10">
           Where is your home located?
         </h2>
 
         <form action="">
           <div className="mb-5">
-            <Select required>
+            <Select required onValueChange={(value) => setLocationValue(value)}>
               <SelectTrigger className="w-full">
                 <SelectValue placeholder="Select a Country" />
               </SelectTrigger>
@@ -36,7 +48,9 @@ export default function AddressRoute() {
               </SelectContent>
             </Select>
           </div>
+          <LazyMap locationValue={locationValue} />
         </form>
+        <BottomBar />
       </div>
     </>
   );
--- app/components/Map.tsx
+++ app/components/Map.tsx
@@ -1,19 +1,34 @@
-import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
-import { useMap } from "react-leaflet/hooks";
+"use client";
 
-const Map = () => {
+import { MapContainer, Marker, TileLayer } from "react-leaflet";
+import { icon } from "leaflet";
+import "leaflet/dist/leaflet.css";
+
+import { useCountries } from "../hooks/getCountries";
+
+const Map = ({ locationValue }: { locationValue: string }) => {
+  const { getCountryByValue } = useCountries();
+  const latLang = getCountryByValue(locationValue)?.latLang;
+
+  const ICON = icon({
+    iconUrl:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQGjeIpitKlqtU_y2C2zdBLaHgP2_5WeWO67WVlIQFz2Q&s",
+    iconSize: [50, 50],
+  });
+
   return (
     <>
-      <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+      <MapContainer
+        center={latLang ?? [51.505, -0.09]}
+        zoom={4}
+        scrollWheelZoom={false}
+        className="h-[50vh] rounded-lg z-0"
+      >
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        <Marker position={[51.505, -0.09]}>
-          <Popup>
-            A pretty CSS3 popup. <br /> Easily customizable.
-          </Popup>
-        </Marker>
+        <Marker position={latLang ?? [51.505, -0.09]} icon={ICON} />
       </MapContainer>
     </>
   );
