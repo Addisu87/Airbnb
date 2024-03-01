f2ce2234eb4346197c7dc0f17bfd0e0b7a48eb32
Addisu <addisuhaile87@gmail.com>
2024-03-01 11:48:48 +0300

    Add a card skeleton

--- app/page.tsx
+++ app/page.tsx
@@ -2,13 +2,20 @@ import prismadb from "@/lib/db";
 
 import Categories from "./components/Categories";
 import ListingCard from "./components/ListingCard";
+import { Suspense } from "react";
+import CardSkeleton from "./components/CardSkeleton";
 
-async function getData() {
+async function getData({
+  searchParams,
+}: {
+  searchParams?: { filter?: string };
+}) {
   const data = await prismadb.home.findMany({
     where: {
       addedCategory: true,
       addedDescription: true,
       addedLocation: true,
+      categoryName: searchParams?.filter ?? undefined,
     },
     select: {
       photo: true,
@@ -21,25 +28,38 @@ async function getData() {
   return data;
 }
 
-const Home = async () => {
-  const data = await getData();
+async function ShowItems({
+  searchParams,
+}: {
+  searchParams?: { filter?: string };
+}) {
+  const data = await getData({ searchParams: searchParams });
+  return (
+    <div className="grid lg:grid-cols-4 sm:grid-cols-2 md:grid-cols-3 gap-8 mt-8">
+      {data.map((item) => (
+        <ListingCard
+          key={item.id}
+          description={item.description as string}
+          imagePath={item.photo as string}
+          location={item.country as string}
+          price={item.price as number}
+        />
+      ))}
+    </div>
+  );
+}
+
+export default function Home({
+  searchParams,
+}: {
+  searchParams?: { filter?: string };
+}) {
   return (
     <div className="container mx-auto px-5 lg:px-10">
       <Categories />
-
-      <div className="grid lg:grid-cols-4 sm:grid-cols-2 md:grid-cols-3 gap-8 mt-8">
-        {data.map((item) => (
-          <ListingCard
-            key={item.id}
-            description={item.description as string}
-            imagePath={item.photo as string}
-            location={item.country as string}
-            price={item.price as number}
-          />
-        ))}
-      </div>
+      <Suspense key={searchParams?.filter} fallback={<CardSkeleton />}>
+        <ShowItems searchParams={searchParams} />
+      </Suspense>
     </div>
   );
-};
-
-export default Home;
+}
